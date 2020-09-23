0d35f70c90ff746dd1f1fde30463b9094084df89
ChibiKev <k3v.ch3n@gmail.com>
2020-09-23 14:49:56 -0400

    Created a blank login page to be modified/designed

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import Buy from './components/MainPages/Buy/Buy';
 import Sell from './components/MainPages/Sell/Sell';
 import Trade from './components/MainPages/Trade/Trade';
 import Swap from './components/MainPages/Swap/Swap';
+import Login from './components/AccessPages/Login/Login';
 import Profile from './components/UserPages/Profile/Profile';
 import Balance from './components/UserPages/Balance/Balance';
 import Message from './components/UserPages/Message/Message';
@@ -32,6 +33,7 @@ class App extends React.Component {
               <Route path="/Sell" component={Sell} />
               <Route path="/Trade" component={Trade} />
               <Route path="/Swap" component={Swap} />
+              <Route path="/Login" component={Login} />
               <Route path="/Profile" component={Profile} />
               <Route path="/Balance" component={Balance} />
               <Route path="/Message" component={Message} />
