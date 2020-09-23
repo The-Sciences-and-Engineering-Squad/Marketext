a07e093ac21655249de646a350dc63396b679aa0
OzunaEddie <immanuelozuna@gmail.com>
2020-09-23 16:03:05 -0400

    connect database to flask

--- server/__init__.py
+++ server/__init__.py
@@ -1,17 +1,26 @@
 import os
-
 from flask import Flask
+from flask_mysqldb import MySQL
 
+
+db = MySQL()
 
 def create_app():
     # create and configure the app
     app = Flask(__name__)
-   
-    # a simple test run
-    @app.route('/hello',methods=['GET'])
-    def hello():
-        return {
-            'userId':1
-        }
+    
+    # database connection credentials
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] ='2641034@Er12'
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_DB'] = 'martketext'
+    app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+    db.init_app(app)
+    
+    from server.controllers import user_controller
+    app.register_blueprint(user_controller.bp)
 
     return app
+
+    
