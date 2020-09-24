821028e6e10214fc819af8dda686574ecddb9fbd
ChibiKev <k3v.ch3n@gmail.com>
2020-09-24 11:34:20 -0400

    Added Placeholder and Classes for Register Page. Makes it look better

--- client/src/components/AccessPages/Register/Register.js
+++ client/src/components/AccessPages/Register/Register.js
@@ -8,22 +8,22 @@ export default class Register extends React.Component {
         <form>
           <div className="form-group">
             <label>Username:</label>
-            <input id="username" type="text"/>
+            <input id="username" type="text" placeholder="Enter Username" className="form-control"/>
           </div>
           <div className="form-group">
             <label>E-mail:</label>
-            <input id="email" type="email"/>
+            <input id="email" type="email" placeholder="Enter Email" className="form-control"/>
           </div>
           <div className="form-group">
             <label>Password:</label>
-            <input id="password" type="password"/>
+            <input id="password" type="password" placeholder="Enter Password" className="form-control"/>
           </div>
           <div className="form-group">
             <label>Confirm Password:</label>
-            <input id="password2" type="password"/>
+            <input id="password2" type="password" placeholder="Reenter Password" className="form-control"/>
           </div>
         </form>
-        <button type="submit">
+        <button type="submit" className="btn btn-danger">
           Register
         </button>
       </div>
