10868a4088c1a62ca22bacad6457a8098b673dd0
ChibiKev <k3v.ch3n@gmail.com>
2020-09-24 11:36:07 -0400

    Added onChange effects and States for Register Page. Store the values for the inputs.

--- client/src/components/AccessPages/Register/Register.js
+++ client/src/components/AccessPages/Register/Register.js
@@ -2,25 +2,44 @@ import React from 'react';
 import './Register.css'
 
 export default class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      password2: "",
+    };
+  }
+
+  // Handle field change
+  handleChange = (input) => (e) => {
+    this.setState({ [input]: e.target.value });
+  };
+
   render() {
     return (
       <div>
         <form>
           <div className="form-group">
             <label>Username:</label>
-            <input id="username" type="text" placeholder="Enter Username" className="form-control"/>
+            <input id="username" type="text" placeholder="Enter Username" className="form-control"
+              onChange={this.handleChange("username")}/>
           </div>
           <div className="form-group">
             <label>E-mail:</label>
-            <input id="email" type="email" placeholder="Enter Email" className="form-control"/>
+            <input id="email" type="email" placeholder="Enter Email" className="form-control"
+              onChange={this.handleChange("email")}/>
           </div>
           <div className="form-group">
             <label>Password:</label>
-            <input id="password" type="password" placeholder="Enter Password" className="form-control"/>
+            <input id="password" type="password" placeholder="Enter Password" className="form-control"
+              onChange={this.handleChange("password")}/>
           </div>
           <div className="form-group">
             <label>Confirm Password:</label>
-            <input id="password2" type="password" placeholder="Reenter Password" className="form-control"/>
+            <input id="password2" type="password" placeholder="Reenter Password" className="form-control"
+              onChange={this.handleChange("password2")}/>
           </div>
         </form>
         <button type="submit" className="btn btn-danger">
