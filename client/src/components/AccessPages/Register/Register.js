377293126abf11d1adbd682d41ea6fd793d4ba65
ChibiKev <k3v.ch3n@gmail.com>
2020-09-24 11:32:49 -0400

    Added Forms for Register Page

--- client/src/components/AccessPages/Register/Register.js
+++ client/src/components/AccessPages/Register/Register.js
@@ -5,7 +5,27 @@ export default class Register extends React.Component {
   render() {
     return (
       <div>
-        This is the Register Page.
+        <form>
+          <div className="form-group">
+            <label>Username:</label>
+            <input id="username" type="text"/>
+          </div>
+          <div className="form-group">
+            <label>E-mail:</label>
+            <input id="email" type="email"/>
+          </div>
+          <div className="form-group">
+            <label>Password:</label>
+            <input id="password" type="password"/>
+          </div>
+          <div className="form-group">
+            <label>Confirm Password:</label>
+            <input id="password2" type="password"/>
+          </div>
+        </form>
+        <button type="submit">
+          Register
+        </button>
       </div>
     );
   }
