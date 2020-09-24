bb7dcc43c1e147256afd036fe39d6ede7fc01373
ChibiKev <k3v.ch3n@gmail.com>
2020-09-24 13:30:02 -0400

    Sized and Positioned for Register Page.

--- client/src/components/AccessPages/Register/Register.js
+++ client/src/components/AccessPages/Register/Register.js
@@ -37,33 +37,35 @@ export default class Register extends React.Component {
 
   render() {
     return (
-      <div className="container bg-primary">
-        <form>
-          <div className="form-group">
-            <label className="text-light">Username:</label>
-            <input id="username" type="text" placeholder="Enter Username" className="form-control"
-              onChange={this.handleChange("username")}/>
-          </div>
-          <div className="form-group">
-            <label className="text-light">E-mail:</label>
-            <input id="email" type="email" placeholder="Enter Email" className="form-control"
-              onChange={this.handleChange("email")}/>
-          </div>
-          <div className="form-group">
-            <label className="text-light">Password:</label>
-            <input id="password" type="password" placeholder="Enter Password" className="form-control"
-              onChange={this.handleChange("password")}/>
-          </div>
-          <div className="form-group">
-            <label className="text-light">Confirm Password:</label>
-            <input id="password2" type="password" placeholder="Reenter Password" className="form-control"
-              onChange={this.handleChange("password2")}/>
-          </div>
-        </form>
-        <button type="submit" className="btn btn-danger btn-lg btn-block"
-          onClick={this.handleSubmit}>
-          Register
-        </button>
+      <div className="center">
+        <div className="container container-bg rounded px-5 py-4">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Username:</label>
+              <input id="username" type="text" placeholder="Enter Username" className="form-control"
+                onChange={this.handleChange("username")}/>
+            </div>
+            <div className="form-group">
+              <label className="text-light">E-mail:</label>
+              <input id="email" type="email" placeholder="Enter Email" className="form-control"
+                onChange={this.handleChange("email")}/>
+            </div>
+            <div className="form-group">
+              <label className="text-light">Password:</label>
+              <input id="password" type="password" placeholder="Enter Password" className="form-control"
+                onChange={this.handleChange("password")}/>
+            </div>
+            <div className="form-group">
+              <label className="text-light">Confirm Password:</label>
+              <input id="password2" type="password" placeholder="Reenter Password" className="form-control"
+                onChange={this.handleChange("password2")}/>
+            </div>
+          </form>
+          <button type="submit" className="btn btn-danger btn-lg btn-block"
+            onClick={this.handleSubmit}>
+            Register
+          </button>
+        </div>
       </div>
     );
   }
