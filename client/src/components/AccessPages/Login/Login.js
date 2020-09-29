61a027518bd5b38b2cc18514eb21f67e419fd648
ChibiKev <k3v.ch3n@gmail.com>
2020-09-29 15:12:34 -0400

    Modified Login to react-bootstrap

--- client/src/components/AccessPages/Login/Login.js
+++ client/src/components/AccessPages/Login/Login.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 import './Login.css'
 
 export default class Login extends React.Component {
@@ -39,7 +43,7 @@ export default class Login extends React.Component {
   render() {
     return (
       <div className="center">
-        <div className="container container-bg rounded px-5 py-4 mx-4">
+        <Container className="container-bg rounded px-5 py-4 mx-4">
         <h2 className="text-light text-center">Login to Your Marketext Account</h2>
           { this.state.errors.length > 0 ?  
             this.state.errors.map((error,index) => {
@@ -48,27 +52,28 @@ export default class Login extends React.Component {
           : 
           <div></div>
           } 
-          <form>
-            <div className="form-group">
-              <label className="text-light">E-mail:</label>
-              <input id="email" type="email" placeholder="Enter Email" className="form-control"
+          <Form>
+            <Form.Group controlId="formEmail">
+              <Form.Label className="text-light">Email Address:</Form.Label>
+              <Form.Control type="email" placeholder="Enter Email" 
                 onChange={this.handleChange("email")}/>
-            </div>
-            <div className="form-group">
-              <label className="text-light">Password:</label>
-              <input id="password" type="password" placeholder="Enter Password" className="form-control"
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label className="text-light">Password:</Form.Label>
+              <Form.Control type="password" placeholder="Enter Password" 
                 onChange={this.handleChange("password")}/>
-            </div>
-          </form>
-          <button type="submit" className="btn btn-danger btn-lg btn-block"
+            </Form.Group>
+          </Form>
+          <Button variant="danger" type="submit" size="lg" block
             onClick={this.handleSubmit}>
             Login
-          </button>
+          </Button>
           <br/>
-          <label className="text-light">Don't have an account? <a href="Register" className="text-danger">Sign Up</a></label>  
-          <br/>
+          <p className="text-light">
+            Don't have an account? <a href="Register" className="text-danger">Sign Up</a>
+          </p>  
           <a href="ForgotPassword" className="text-danger">Forgot Your Password?</a>
-        </div>
+        </Container>
       </div>
     );
   }
