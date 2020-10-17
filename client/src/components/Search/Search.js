4310038acee63938c9eea6dd847217a47ef1e5d9
ChibiKev <k3v.ch3n@gmail.com>
2020-10-17 00:29:27 -0400

    Modified Searchbar

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react'
-import { Form, Button, FormGroup, FormControl, ControlLabel, InputGroup } from "react-bootstrap";
+import { Form, Button, FormControl, InputGroup } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
 import './Search.css'
 
 export default class Search extends Component {
-    render() {
-        return (
-            <div className="search-box-container" >
-                <InputGroup className="mb-3">
-                    <FormControl
-                        placeholder="Search by ISBN, title or author's name"
-                        aria-label="Search by ISBN, title or author's name"
-                        aria-describedby="basic-addon2"
-                    />
-                    <InputGroup.Append>
-                        <Button variant="outline-secondary"><FontAwesomeIcon icon={faSearch} /></Button>
-                    </InputGroup.Append>
-                </InputGroup>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Form>
+        <InputGroup className="searchbar pt-4">
+          <FormControl
+            placeholder="Search by ISBN, title or author's name"
+            aria-label="Search by ISBN, title or author's name"
+            aria-describedby="basic-addon2"
+          />
+          <InputGroup.Append>
+            <Button variant="outline-secondary"><FontAwesomeIcon icon={faSearch} /></Button>
+          </InputGroup.Append>
+        </InputGroup>
+      </Form>
+    )
+  }
 }
