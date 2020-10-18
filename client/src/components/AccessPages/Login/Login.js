390122d1088e9d29f5d0b25c7ca6a44e0d667e6c
ChibiKev <k3v.ch3n@gmail.com>
2020-10-18 15:12:57 -0400

    Changed Login to Username instead of Email

--- client/src/components/AccessPages/Login/Login.js
+++ client/src/components/AccessPages/Login/Login.js
@@ -12,7 +12,7 @@ export default class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: "",
+      username: "",
       password: "",
       errors: [],
     };
@@ -25,11 +25,11 @@ export default class Login extends React.Component {
 
   handleSubmit = (e) => {
     e.preventDefault(); 
-    const { email, password } = this.state;
+    const { username, password } = this.state;
     var newState = Object.assign({}, this.state);
     newState.errors = [];
-    if (email === "") {
-      newState.errors.push("Please Enter an Email");
+    if (username === "") {
+      newState.errors.push("Please Enter an Username");
     }
     if (password === "") {
       newState.errors.push("Please Enter a Password");
@@ -78,10 +78,10 @@ export default class Login extends React.Component {
           <div></div>
           } 
           <Form onSubmit={this.handleSubmit}>
-            <Form.Group controlId="formEmail">
-              <Form.Label className="text-light">Email Address:</Form.Label>
-              <Form.Control type="email" placeholder="Enter Email" 
-                onChange={this.handleChange("email")}/>
+            <Form.Group controlId="formUsername">
+              <Form.Label className="text-light">Username:</Form.Label>
+              <Form.Control type="text" placeholder="Enter Username" 
+                onChange={this.handleChange("username")}/>
             </Form.Group>
             <Form.Group controlId="formPassword">
               <Form.Label className="text-light">Password:</Form.Label>
