60a0a387110566d3b022b56513a1ce1b60c0dff9
ChibiKev <k3v.ch3n@gmail.com>
2020-10-18 15:01:40 -0400

    Placement of Sidebar - Balance Page

--- client/src/components/UserPages/Profile/Profile.js
+++ client/src/components/UserPages/Profile/Profile.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Sidebar from '../../Sidebar/Sidebar'
-import './Profile.css'
+import Sidebar from '../../Sidebar/Sidebar';
+
+import './Profile.css';
 
 export default class Profile extends React.Component {
   render() {
