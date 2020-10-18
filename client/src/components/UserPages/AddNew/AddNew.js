4634cc5206dc177bf2a18818fdced5156854cdf8
ChibiKev <k3v.ch3n@gmail.com>
2020-10-18 15:05:48 -0400

    Placement of Sidebar - Add New Page

--- client/src/components/UserPages/AddNew/AddNew.js
+++ client/src/components/UserPages/AddNew/AddNew.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Sidebar from '../../Sidebar/Sidebar';
+
 import './AddNew.css'
 
 export default class AddNew extends React.Component {
   render() {
     return (
-      <div>
-        This is the Add New Page.
-      </div>
+      <Container fluid>
+        <Row>
+          <Col xs="4" sm="3" md="2">      
+            <Sidebar />
+          </Col>
+          <Col xs="8" sm="9" md="10">
+            This is the Add New Page
+          </Col> 
+        </Row>
+      </Container>
     );
   }
 }
