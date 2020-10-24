5c91371dad2ca1d9f12f487f18b4a5fa256d7317
OzunaEddie <immanuelozuna@gmail.com>
2020-10-24 00:59:48 -0400

    Forgot Password API

--- server/__init__.py
+++ server/__init__.py
@@ -20,6 +20,16 @@ def create_app():
     app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
     app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
     app.config['SESSION_COOKIE_SAMESITE'] = "Strict"
+
+    app.config.update(
+        MAIL_SERVER =  os.getenv("MAIL_SERVER"),
+        MAIL_PORT = os.getenv("MAIL_PORT"),
+        MAIL_USE_SSL = True,
+        MAIL_USERNAME = os.getenv("MAIL_USERNAME"),
+        MAIL_PASSWORD = os.getenv("MAIL_PASSWORD"),
+        MAIL_DEFAULT_SENDER=os.getenv("MAIL_DEFAULT_SENDER")
+    )   
+
     db.init_app(app)
     
     from server.controllers import user_controller
