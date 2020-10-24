4687c46b77889d0f21553a33b4fb69c4793d9b0c
du <eftmail@gmail.com>
2020-10-24 16:39:03 -0400

    User circle, clickable, showing different functions for useres - Navbar

--- client/src/components/Navbar1/Navbar1.js
+++ client/src/components/Navbar1/Navbar1.js
@@ -11,7 +11,17 @@ import './Navbar1.css'
 import Cookies from 'universal-cookie';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import Dropdown from 'react-bootstrap/Dropdown'
 // This Navbar is for the Home/Buy/Sell/Trade/Swap Pages.
+
+const CustomToggle = React.forwardRef(({ onClick }, ref) => (
+  <a href="" ref={ref} onClick={(e) => {
+    e.preventDefault()
+    onClick(e)
+  }}>
+    <FontAwesomeIcon size="lg" icon={faUserCircle} />
+  </a>
+));
 
 function Navbar1(props) {
   // This is what you need to use to get the cookies!
@@ -19,8 +29,8 @@ function Navbar1(props) {
   const username = cookies.get('username');
   console.log(username)
   return (
-    <Navbar variant="dark" expand="lg" sticky="top">
-      <Navbar.Brand as={Link} to="/">
+    <Navbar expand="lg" sticky="top">
+      <Navbar.Brand as={Link} to="/" className="nav-basic">
         <img
           alt=""
           src={require('../../public/logo192.png')}
@@ -33,17 +43,40 @@ function Navbar1(props) {
       <Navbar.Toggle />
       <Navbar.Collapse>
         <Nav className="nav m-auto">
-          <Nav.Link as={NavLink} exact to="/" style={{ marginRight: 30, marginLeft: 30 }}>Home</Nav.Link>
-          <Nav.Link as={NavLink} to="/Buy" style={{ marginRight: 30, marginLeft: 30 }}>Buy</Nav.Link>
-          <Nav.Link as={NavLink} to="/Sell" style={{ marginRight: 30, marginLeft: 30 }}>Sell</Nav.Link>
-          <Nav.Link as={NavLink} to="/Trade" style={{ marginRight: 30, marginLeft: 30 }}>Trade</Nav.Link>
-          <Nav.Link as={NavLink} to="/Swap" style={{ marginRight: 30, marginLeft: 30 }}>Swap</Nav.Link>
+          <Nav.Link as={NavLink} exact to="/" className="nav-basic" style={{ marginRight: 30, marginLeft: 30 }}>Home</Nav.Link>
+          <Nav.Link as={NavLink} to="/Buy" className="nav-basic" style={{ marginRight: 30, marginLeft: 30 }}>Buy</Nav.Link>
+          <Nav.Link as={NavLink} to="/Sell" className="nav-basic" style={{ marginRight: 30, marginLeft: 30 }}>Sell</Nav.Link>
+          <Nav.Link as={NavLink} to="/Trade" className="nav-basic" style={{ marginRight: 30, marginLeft: 30 }}>Trade</Nav.Link>
+          <Nav.Link as={NavLink} to="/Swap" className="nav-basic" style={{ marginRight: 30, marginLeft: 30 }}>Swap</Nav.Link>
         </Nav>
         {username ?
           <Nav className="nav">
-            <Nav.Link href="/profile">
-              <FontAwesomeIcon size="lg" icon={faUserCircle} />
-            </Nav.Link>
+            <Dropdown>
+              <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
+              </Dropdown.Toggle>
+              <Dropdown.Menu alignRight="true">
+              <Dropdown.Item eventKey="1">Signed in as {username}<hr/></Dropdown.Item>
+                <Nav>
+                  <Nav.Link href="/Profile" className="nav-dropdownmenu">Your Profile</Nav.Link>
+                </Nav>
+                <Nav>
+                  <Nav.Link href="/Messages" className="nav-dropdownmenu">Your Messages</Nav.Link>
+                </Nav>
+                <Nav>
+                  <Nav.Link href="/Balance" className="nav-dropdownmenu">Your Balance</Nav.Link>
+                </Nav>
+                <Nav>
+                  <Nav.Link href="/CurrentlyListed" className="nav-dropdownmenu">Your Currently Listed</Nav.Link>
+                </Nav>
+                <Nav>
+                  <Nav.Link href="/TransactionHistory" className="nav-dropdownmenu">Your Transaction History</Nav.Link>
+                </Nav>
+                <hr/>
+                <Nav>
+                  <Nav.Link href="/Login" className="nav-dropdownmenu">Sign Out</Nav.Link>
+                </Nav>
+              </Dropdown.Menu>
+            </Dropdown>
           </Nav> :
           <Nav className="nav">
             <Nav.Link href="/Login">Sign In</Nav.Link>
