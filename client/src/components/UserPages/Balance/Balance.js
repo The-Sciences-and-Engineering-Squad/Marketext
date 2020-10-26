0b4cf7d867a2fd5abfc84eb0840f8c79cd8b00d1
ChibiKev <k3v.ch3n@gmail.com>
2020-10-26 15:56:19 -0400

    Added Submit Logic + States and Functions

--- client/src/components/UserPages/Balance/Balance.js
+++ client/src/components/UserPages/Balance/Balance.js
@@ -11,6 +11,58 @@ import Button from 'react-bootstrap/Button';
 import './Balance.css'
 
 export default class Balance extends React.Component {
+  componentDidMount(){
+    // Replace this information with information retrieved from the backend about the user balance.
+    this.setState({
+      currentBalance: 100,
+    })
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentBalance: "",
+      addBalance: "",
+      subtractBalance: "",
+      errors: [],
+    };
+  }
+
+  // Handle field change
+  handleChange = (input) => (e) => {
+    this.setState({ [input]: e.target.value });
+  };
+
+  // eventually api call to call the backend
+  handleAdd = (e) => {
+    e.preventDefault();
+    var newState = Object.assign({}, this.state);
+    newState.errors = [];
+    if (this.state.addBalance < 0){
+      newState.errors.push("$" + this.state.addBalance + " Is A Negative Balance. Please Enter A Correct Amount To Add!");
+    }
+    else{
+      // Insert backend to add value to user's balance.
+      console.log(this.state.addBalance);
+    }
+    this.setState(newState);
+  }
+
+  // eventually api call to call the backend
+  handleSubtract = (e) => {
+    e.preventDefault();
+    var newState = Object.assign({}, this.state);
+    newState.errors = [];
+    if (this.state.subtractBalance < 0){
+      newState.errors.push("$" + this.state.subtractBalance + " Is A Negative Deposit. Please Enter A Correct Amount To Deposit!");
+    }
+    else{
+      // Insert backend to subtract value from user's balance.
+      console.log(this.state.subtractBalance);
+    }
+    this.setState(newState);
+  }
+
   render() {
     return (
       <Container fluid>
@@ -22,37 +74,48 @@ export default class Balance extends React.Component {
             <Container fluid>
               <Row className="mt-4 justify-content-center">
                 <Col xs="auto">
-                  <h3>Current Balance: $20</h3>
+                  <h3>Current Balance: ${this.state.currentBalance}</h3>
                 </Col>
               </Row>
               <hr />
+              { this.state.errors.length > 0 ?
+                this.state.errors.map((error,index) => {
+                  return <li key={index} className="text-warning"> {error} </li>
+              })
+              :
+              <div></div>
+              }
               <Row className="py-4">
-                <Form as={Col} sm="12" md="6">
-                  <label>Add to Balance:</label>
-                  <InputGroup>
-                    <InputGroup.Prepend>
-                      <InputGroup.Text>$</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl type="number" min="0.00" step="0.01" placeholder="Amount" />
-                    <InputGroup.Append>
-                      <Button variant="danger" type="submit">Add</Button>
-                    </InputGroup.Append>
-                  </InputGroup>
-                </Form>
+                <Col sm="12" md="6">
+                  <Form onSubmit={this.handleAdd}>
+                    <label>Add to Balance:</label>
+                    <InputGroup>
+                      <InputGroup.Prepend>
+                        <InputGroup.Text>$</InputGroup.Text>
+                      </InputGroup.Prepend>
+                      <FormControl type="number" min="0.00" step="0.01" placeholder="Amount" value={this.state.addBalance} onChange={this.handleChange("addBalance")}/>
+                      <InputGroup.Append>
+                        <Button variant="danger" type="submit" onClick={this.handleAdd}>Add</Button>
+                      </InputGroup.Append>
+                    </InputGroup>
+                  </Form>
+                </Col>
               </Row>
               <Row className="py-4">
-                <Form as={Col} sm="12" md="6">
-                  <label>Deposit to Account:</label>
-                  <InputGroup>
-                    <InputGroup.Prepend>
-                      <InputGroup.Text>$</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl type="number" min="0.00" step="0.01" placeholder="Amount" />
-                    <InputGroup.Append>
-                      <Button variant="danger" type="submit">Deposit</Button>
-                    </InputGroup.Append>
-                  </InputGroup>
-                </Form>
+                <Col sm="12" md="6">
+                  <Form onSubmit={this.handleSubtract}>
+                    <label>Deposit to Account:</label>
+                    <InputGroup>
+                      <InputGroup.Prepend>
+                        <InputGroup.Text>$</InputGroup.Text>
+                      </InputGroup.Prepend>
+                      <FormControl type="number" min="0.00" step="0.01" placeholder="Amount" value={this.state.subtractBalance} onChange={this.handleChange("subtractBalance")}/>
+                      <InputGroup.Append>
+                        <Button variant="danger" type="submit" onClick={this.handleSubtract}>Deposit</Button>
+                      </InputGroup.Append>
+                    </InputGroup>
+                  </Form>
+                </Col>
               </Row>
             </Container>
           </Col> 
