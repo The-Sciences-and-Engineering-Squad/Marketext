dedebb8522eda2b06f000825b16c54370a33def3
ChibiKev <k3v.ch3n@gmail.com>
2020-10-27 21:35:42 -0400

    Provided Skeleton for Message

--- client/src/components/UserPages/Message/Message.js
+++ client/src/components/UserPages/Message/Message.js
@@ -3,9 +3,11 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Sidebar from '../../Sidebar/Sidebar';
-import InputGroup from 'react-bootstrap/InputGroup'
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
+import Tab from 'react-bootstrap/Tab';
+import ListGroup from 'react-bootstrap/ListGroup';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
 import './Message.css'
 
@@ -14,11 +16,51 @@ export default class Message extends React.Component {
     return (
       <Container fluid className="hello">
         <Row>
-          <Col className="side" xs="4" sm="3" md="2">
+          <Col xs="4" sm="3" md="2">
             <Sidebar />
           </Col>
-
-          <Col className="messageHistory" xs="4" sm="3" md="2">
+          <Col xs="8" sm="9" md="10" className="message-container">
+            <Tab.Container defaultActiveKey="link1">
+              <Row>
+                <Col xs={2}>
+                  <ListGroup>
+                    <ListGroup.Item>
+                      Messages
+                    </ListGroup.Item>
+                    <ListGroup.Item action eventKey="link1">
+                      Link 1
+                    </ListGroup.Item>
+                    <ListGroup.Item action eventKey="link2">
+                      Link 2
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+                <Col xs={8}>
+                  <Tab.Content>
+                    <Tab.Pane eventKey="link1">
+                      <Container>
+                        <Row>
+                          <Col>
+                            Test
+                          </Col>
+                        </Row>
+                      </Container>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="link2">
+                      <Container>
+                        <Row>
+                          <Col>
+                            Test2
+                          </Col>
+                        </Row>
+                      </Container>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Col>
+              </Row>
+            </Tab.Container>
+          </Col>
+          {/* <Col className="messageHistory" xs="4" sm="3" md="2">
             <Row className="messageBox">
               <h3 className="MessageHeader">Messages</h3>
             </Row>
@@ -50,7 +92,7 @@ export default class Message extends React.Component {
                 <Button variant="danger" className="sendMessageButton"/>
               </InputGroup>
             </Row>
-          </Col>
+          </Col> */}
         </Row>
       </Container>
     );
