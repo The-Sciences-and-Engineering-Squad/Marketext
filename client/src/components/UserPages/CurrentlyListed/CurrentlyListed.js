8a7242ff447c8800686a83a54f54c7078072ab47
ChibiKev <k3v.ch3n@gmail.com>
2020-11-07 00:08:59 -0500

    Created Table for Currently Listed

--- client/src/components/UserPages/CurrentlyListed/CurrentlyListed.js
+++ client/src/components/UserPages/CurrentlyListed/CurrentlyListed.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Sidebar from '../../Sidebar/Sidebar';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 
+import Sidebar from '../../Sidebar/Sidebar';
 import './CurrentlyListed.css'
 
 export default class CurrentlyListed extends React.Component {
@@ -15,7 +17,54 @@ export default class CurrentlyListed extends React.Component {
             <Sidebar />
           </Col>
           <Col xs="8" sm="9" md="10">
-            This is the Currently Listed Page
+            <Container fluid>
+              <Row className="mt-4 justify-content-center">
+                <Col xs="auto">
+                  <h3>Your Currently Listed Textbooks</h3>
+                </Col>
+              </Row>
+              <hr />
+              <Row className="py-3">
+                <Col xs="12">
+                  <Table bordered hover responsive>
+                    <thead>
+                      <tr>
+                        <th>
+                          Textbook:
+                        </th>
+                        <th>
+                          Category:
+                        </th>
+                        <th>
+                          Price:
+                        </th>
+                        <th>
+                          Remove:
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          Art 100
+                        </td>
+                        <td>
+                          Sell
+                        </td>
+                        <td>
+                          $100
+                        </td>
+                        <td className="text-center">
+                          <Button variant="danger">
+                            X
+                          </Button>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                </Col>
+              </Row>
+            </Container>
           </Col> 
         </Row>
       </Container>
