b5537901953da66b86ba5aa89ca459972b0244f5
ChibiKev <k3v.ch3n@gmail.com>
2020-11-21 02:11:52 -0500

    Added Render for Trade

--- client/src/components/MainPages/Trade/Trade.js
+++ client/src/components/MainPages/Trade/Trade.js
@@ -28,8 +28,8 @@ export default class Trade extends React.Component {
     })
   }
 
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       search: "",
       textbooks: [],
