99034f11cd67b83d85c013757bdd5a53f742ea6b
albfelix31 <albert.felix31@gmail.com>
2020-12-06 17:16:00 -0500

    modifications to app.py file

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 '''server/app.py - main api app declaration'''
+import os
+from dotenv import load_dotenv
 from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
+from flask_mysqldb import MySQL
 
 '''Main wrapper for app creation'''
 app = Flask(__name__, static_folder='./build', static_url_path='/')
 CORS(app)
 
-##
-# API routes
-##
-##
+load_dotenv()
+
+
+db = MySQL()
 
 
 @app.route('/api/items')
@@ -17,22 +20,11 @@ def items():
     '''Sample API route for data'''
     return jsonify([{'title': 'A'}, {'title': 'B'}])
 
-##
-# View route
-##
 
-
-# @app.route('/', defaults={'path': ''})
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
 
-
-# @app.route('/<path:path>')
-# def index(path):
-#     '''Return index.html for all non-api routes'''
-#     #pylint: disable=unused-argument
-#     return send_from_directory(app.static_folder, 'index.html')
 
 @app.errorhandler(404)
 def not_found(e):
