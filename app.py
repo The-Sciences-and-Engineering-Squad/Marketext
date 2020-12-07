55117a2dbc9d92cf8fc965beb0f5bd33ff3eacfb
albfelix31 <albert.felix31@gmail.com>
2020-12-06 20:38:17 -0500

    modifications to app.py file

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 '''server/app.py - main api app declaration'''
+from server.controllers import transaction_controller
+from server.controllers import contact_controller
+from server.controllers import profile_controller
+from server.controllers import currently_controller
+from server.controllers import balance_controller
+from server.controllers import books_controller
+from server.controllers import user_controller
 import os
 # from dotenv import load_dotenv
 from flask import Flask, jsonify
@@ -33,26 +40,19 @@ app.config.update(
 
 # db.init_app(app)
 #
-# from server.controllers import user_controller
-# app.register_blueprint(user_controller.bp)
-#
-# from server.controllers import books_controller
-# app.register_blueprint(books_controller.bp)
-#
-# from server.controllers import balance_controller
-# app.register_blueprint(balance_controller.bp)
-#
-# from server.controllers import currently_controller
-# app.register_blueprint(currently_controller.bp)
-#
-# from server.controllers import profile_controller
-# app.register_blueprint(profile_controller.bp)
-#
-# from server.controllers import contact_controller
-# app.register_blueprint(contact_controller.bp)
-#
-# from server.controllers import transaction_controller
-# app.register_blueprint(transaction_controller.bp)
+app.register_blueprint(user_controller.bp)
+
+app.register_blueprint(books_controller.bp)
+
+app.register_blueprint(balance_controller.bp)
+
+app.register_blueprint(currently_controller.bp)
+
+app.register_blueprint(profile_controller.bp)
+
+app.register_blueprint(contact_controller.bp)
+
+app.register_blueprint(transaction_controller.bp)
 
 
 @app.route('/api/items')
