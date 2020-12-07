01c6fcf247f5ae0364e96c2cfc2aa39eef8651b7
albfelix31 <albert.felix31@gmail.com>
2020-12-06 20:15:32 -0500

    modifications to app.py file

--- app.py
+++ app.py
@@ -1,50 +1,15 @@
 '''server/app.py - main api app declaration'''
-from server.controllers import transaction_controller
-from server.controllers import contact_controller
-from server.controllers import profile_controller
-from server.controllers import currently_controller
-from server.controllers import balance_controller
-from server.controllers import books_controller
-from server.controllers import user_controller
-import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
 
 '''Main wrapper for app creation'''
-# create and configure the app
 app = Flask(__name__, static_folder='./build', static_url_path='/')
 CORS(app)
-app.config['SECRET_KEY'] = os.urandom(12)
-# database connection credentials
-app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
-app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
-app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
-app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-app.config['SESSION_COOKIE_SAMESITE'] = "Strict"
 
-app.config.update(
-    MAIL_SERVER=os.getenv("MAIL_SERVER"),
-    MAIL_PORT=os.getenv("MAIL_PORT"),
-    MAIL_USE_SSL=True,
-    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
-    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
-    MAIL_DEFAULT_SENDER=os.getenv("MAIL_DEFAULT_SENDER")
-)
-
-app.register_blueprint(user_controller.bp)
-
-app.register_blueprint(books_controller.bp)
-
-app.register_blueprint(balance_controller.bp)
-
-app.register_blueprint(currently_controller.bp)
-
-app.register_blueprint(profile_controller.bp)
-
-app.register_blueprint(contact_controller.bp)
-
-app.register_blueprint(transaction_controller.bp)
+##
+# API routes
+##
+##
 
 
 @app.route('/api/items')
@@ -52,11 +17,22 @@ def items():
     '''Sample API route for data'''
     return jsonify([{'title': 'A'}, {'title': 'B'}])
 
+##
+# View route
+##
 
+
+# @app.route('/', defaults={'path': ''})
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
 
+
+# @app.route('/<path:path>')
+# def index(path):
+#     '''Return index.html for all non-api routes'''
+#     #pylint: disable=unused-argument
+#     return send_from_directory(app.static_folder, 'index.html')
 
 @app.errorhandler(404)
 def not_found(e):
