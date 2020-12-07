c326ba52c2f295664a55ac2b39358159f9536bf0
albfelix31 <albert.felix31@gmail.com>
2020-12-07 00:56:50 -0500

    modifications to app.py file

--- server/__init__.py
+++ server/__init__.py
@@ -1,17 +1,17 @@
-from server.controllers import transaction_controller
-from server.controllers import contact_controller
-from server.controllers import profile_controller
-from server.controllers import currently_controller
-from server.controllers import balance_controller
-from server.controllers import books_controller
-from server.controllers import user_controller
+# from server.controllers import transaction_controller
+# from server.controllers import contact_controller
+# from server.controllers import profile_controller
+# from server.controllers import currently_controller
+# from server.controllers import balance_controller
+# from server.controllers import books_controller
+# from server.controllers import user_controller
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 from flask import Flask
-from flaskext.mysql import MySQL
+from flask_mysqldb import MySQL
 
 
-# load_dotenv()
+load_dotenv()
 
 
 db = MySQL()
@@ -20,20 +20,20 @@ db = MySQL()
 app = Flask(__name__, static_folder='../client/build', static_url_path='/')
 app.config['SECRET_KEY'] = os.urandom(12)
 # database connection credentials
-# app.config['MYSQL_USER'] = 'b2393fd53cce34'
-# app.config['MYSQL_PASSWORD'] = 'bbab5204'
-# app.config['MYSQL_HOST'] = 'us-cdbr-east-02.cleardb.com'
-# app.config['MYSQL_DB'] = 'heroku_c28054944c75e38'
-# app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-# app.config['SESSION_COOKIE_SAMESITE'] = "Strict"
-
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'b2393fd53cce34'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'bbab5204'
-app.config['MYSQL_DATABASE_DB'] = 'heroku_c28054944c75e38'
-app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-02.cleardb.com'
+app.config['MYSQL_USER'] = 'b2393fd53cce34'
+app.config['MYSQL_PASSWORD'] = 'bbab5204'
+app.config['MYSQL_HOST'] = 'us-cdbr-east-02.cleardb.com'
+app.config['MYSQL_DB'] = 'heroku_c28054944c75e38'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 app.config['SESSION_COOKIE_SAMESITE'] = "Strict"
+
+# MySQL configurations
+# app.config['MYSQL_DATABASE_USER'] = 'b2393fd53cce34'
+# app.config['MYSQL_DATABASE_PASSWORD'] = 'bbab5204'
+# app.config['MYSQL_DATABASE_DB'] = 'heroku_c28054944c75e38'
+# app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-02.cleardb.com'
+# app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# app.config['SESSION_COOKIE_SAMESITE'] = "Strict"
 
 app.config.update(
     MAIL_SERVER=os.getenv("MAIL_SERVER"),
@@ -46,16 +46,16 @@ app.config.update(
 
 db.init_app(app)
 
-app.register_blueprint(user_controller.bp)
-
-app.register_blueprint(books_controller.bp)
-
-app.register_blueprint(balance_controller.bp)
-
-app.register_blueprint(currently_controller.bp)
-
-app.register_blueprint(profile_controller.bp)
-
-app.register_blueprint(contact_controller.bp)
-
-app.register_blueprint(transaction_controller.bp)
+# app.register_blueprint(user_controller.bp)
+#
+# app.register_blueprint(books_controller.bp)
+#
+# app.register_blueprint(balance_controller.bp)
+#
+# app.register_blueprint(currently_controller.bp)
+#
+# app.register_blueprint(profile_controller.bp)
+#
+# app.register_blueprint(contact_controller.bp)
+#
+# app.register_blueprint(transaction_controller.bp)
