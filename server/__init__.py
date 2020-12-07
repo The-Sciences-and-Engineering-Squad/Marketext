9d5013341a176a4d6037a98c83864409bb5fd96b
albfelix31 <albert.felix31@gmail.com>
2020-12-07 00:04:11 -0500

    modifications to app.py file

--- server/__init__.py
+++ server/__init__.py
@@ -20,10 +20,18 @@ db = MySQL()
 app = Flask(__name__, static_folder='../client/build', static_url_path='/')
 app.config['SECRET_KEY'] = os.urandom(12)
 # database connection credentials
-app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
-app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
-app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
-app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
+# app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
+# app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
+# app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
+# app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
+# app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# app.config['SESSION_COOKIE_SAMESITE'] = "Strict"
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'b2393fd53cce34'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'bbab5204'
+app.config['MYSQL_DATABASE_DB'] = 'heroku_c28054944c75e38'
+app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-02.cleardb.com'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 app.config['SESSION_COOKIE_SAMESITE'] = "Strict"
 
