e894ec148508d7b4d77cc9344d08fb3ce5e99268
albfelix31 <albert.felix31@gmail.com>
2020-12-06 22:16:45 -0500

    modifications to app.py file

--- app.py
+++ app.py
@@ -1,29 +1,11 @@
 '''server/app.py - main api app declaration'''
 import os
-from flask import Flask, jsonify
+from flask import jsonify
 from flask_cors import CORS
-
+from server import app
 '''Main wrapper for app creation'''
 # create and configure the app
-app = Flask(__name__, static_folder='./build', static_url_path='/')
 CORS(app)
-app.config['SECRET_KEY'] = os.urandom(12)
-# database connection credentials
-app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
-app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
-app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
-app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-app.config['SESSION_COOKIE_SAMESITE'] = "Strict"
-
-app.config.update(
-    MAIL_SERVER=os.getenv("MAIL_SERVER"),
-    MAIL_PORT=os.getenv("MAIL_PORT"),
-    MAIL_USE_SSL=True,
-    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
-    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
-    MAIL_DEFAULT_SENDER=os.getenv("MAIL_DEFAULT_SENDER")
-)
 
 
 @app.route('/api/items')
