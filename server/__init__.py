6979b409f8a25b27770c073023946fdd958ce656
albfelix31 <albert.felix31@gmail.com>
2020-12-07 11:19:59 -0500

    modifications to app.py file

--- server/__init__.py
+++ server/__init__.py
@@ -1,10 +1,10 @@
-from server.controllers import transaction_controller
-from server.controllers import contact_controller
-from server.controllers import profile_controller
-from server.controllers import currently_controller
-from server.controllers import balance_controller
-from server.controllers import books_controller
-from server.controllers import user_controller
+# from server.controllers import transaction_controller
+# from server.controllers import contact_controller
+# from server.controllers import profile_controller
+# from server.controllers import currently_controller
+# from server.controllers import balance_controller
+# from server.controllers import books_controller
+# from server.controllers import user_controller
 import os
 from dotenv import load_dotenv
 from flask import Flask
@@ -39,18 +39,18 @@ def create_app():
 
     db.init_app(app)
 
-    app.register_blueprint(user_controller.bp)
-
-    app.register_blueprint(books_controller.bp)
-
-    app.register_blueprint(balance_controller.bp)
-
-    app.register_blueprint(currently_controller.bp)
-
-    app.register_blueprint(profile_controller.bp)
-
-    app.register_blueprint(contact_controller.bp)
-
-    app.register_blueprint(transaction_controller.bp)
+    # app.register_blueprint(user_controller.bp)
+    #
+    # app.register_blueprint(books_controller.bp)
+    #
+    # app.register_blueprint(balance_controller.bp)
+    #
+    # app.register_blueprint(currently_controller.bp)
+    #
+    # app.register_blueprint(profile_controller.bp)
+    #
+    # app.register_blueprint(contact_controller.bp)
+    #
+    # app.register_blueprint(transaction_controller.bp)
 
     return app
