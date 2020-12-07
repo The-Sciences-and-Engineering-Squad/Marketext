0ba90b222873c7747c0a7dc75a89271154dfff19
albfelix31 <albert.felix31@gmail.com>
2020-12-06 20:18:21 -0500

    modifications to app.py file

--- app.py
+++ app.py
@@ -6,33 +6,19 @@ from flask_cors import CORS
 app = Flask(__name__, static_folder='./build', static_url_path='/')
 CORS(app)
 
-##
-# API routes
-##
-##
-
 
 @app.route('/api/items')
 def items():
     '''Sample API route for data'''
     return jsonify([{'title': 'A'}, {'title': 'B'}])
 
-##
-# View route
-##
-
-
 # @app.route('/', defaults={'path': ''})
+
+
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
 
-
-# @app.route('/<path:path>')
-# def index(path):
-#     '''Return index.html for all non-api routes'''
-#     #pylint: disable=unused-argument
-#     return send_from_directory(app.static_folder, 'index.html')
 
 @app.errorhandler(404)
 def not_found(e):
